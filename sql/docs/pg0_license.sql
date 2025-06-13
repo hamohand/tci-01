ec8417935a8378d977e4c934a478136fee994011
Muhend <hamohand@gmail.com>
2025-06-13 11:24:54 +0200

    Suppression du dossier 'sql' du suivi Git et ajout à .gitignore

--- sql/docs/pg0_license.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS license;
-
-CREATE TABLE license (
-     id BIGSERIAL PRIMARY KEY,
-     activation_date TIMESTAMP WITHOUT TIME ZONE, -- Correspond à activationDate
-     valid_until TIMESTAMP WITHOUT TIME ZONE      -- Correspond à validUntil
-    -- Ajoutez d'autres contraintes ou colonnes si votre entité License en a plus.
-);
